5b34056287d4292689b7fa3c4cc3643ee097f766
Sampurna <sampurna@banbagroup.com>
2019-06-22 16:52:39 +0545

    read dictionary.txt file and keep them in array

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -8,6 +8,16 @@ class Dictionary
     return [] if digits.nil? || digits.length != 10 || digits.split('').select{|a|(a.to_i == 0 || a.to_i == 1)}.length > 0
     letters = {"2" => ["a", "b", "c"],"3" => ["d", "e", "f"],"4" => ["g", "h", "i"],"5" => ["j", "k", "l"],"6" => ["m", "n", "o"],"7" => ["p", "q", "r", "s"],"8" => ["t", "u", "v"],"9" => ["w", "x", "y", "z"]}
 
+    dictionary = {}
+    for i in (1..30)
+      dictionary[i] = []
+    end
+    
+    file_name = "dictionary.txt"
+    File.foreach( file_name ) do |word|
+      dictionary[word.length] << word.chop.to_s.downcase
+    end
+
   end
 
 end
